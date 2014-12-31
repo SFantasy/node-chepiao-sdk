e74455f8898712c93b3bb6c4925b3e7815b60ecf
Fantasy Shao <forever.fantasy27@gmail.com>
2014-12-31 14:17:08 +0800

    Complete two API in the SDK.

--- index.js
+++ index.js
@@ -19,17 +19,41 @@ var Chepiao = function(config) {
 
 module.exports = Chepiao;
 
+/**
+ * Util function
+ * @param url
+ * @param form
+ * @param callback
+ */
 Chepiao.prototype.request = function(url, form, callback) {
   var _this = this;
 
   request.post({
     url: url,
-    formData: objectAssign(_this.config, form)
+    formData: objectAssign(_this.config, form),
+    json: true
   }, function (err, res, body) {
-    if (!err) callback(body);
+    if (!err && res.statusCode === 200) {
+      if (body.errMsg === 'Y') {
+        callback({
+          success: true,
+          data: body.data
+        });
+      } else {
+        callback({
+          success: false,
+          data: []
+        });
+      }
+    }
   });
 };
 
+/**
+ * Get the schedule of a specified railway
+ * @param number
+ * @param callback
+ */
 Chepiao.prototype.schedule = function (number, callback) {
 
   this.request(api.schedule, {
@@ -38,4 +62,22 @@ Chepiao.prototype.schedule = function (number, callback) {
     callback(data);
   });
 
+};
+
+/**
+ * Get the left tickets info with specified params
+ * @param date
+ * @param startStation
+ * @param arriveStation
+ * @param callback
+ */
+Chepiao.prototype.leftTickets = function (date, startStation, arriveStation, callback) {
+
+  this.request(api.leftTickets, {
+    'date': date,
+    'startStation': startStation,
+    'arriveStation': arriveStation
+  }, function (data) {
+    callback(data);
+  });
 };
