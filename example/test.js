72f6e48d6f18c478c26011538d2a27f7d5315a5f
Fantasy Shao <forever.fantasy27@gmail.com>
2015-01-04 15:32:08 +0800

    Update project.

--- example/test.js
+++ example/test.js
@@ -5,7 +5,7 @@
  * @description
  * @author Fantasy <fantasyshao@icloud.com>
  * @create 2014-12-31
- * @update 2014-12-31
+ * @update 2014-01-03
  */
 
 var Chepiao = require('../index');
@@ -17,6 +17,10 @@ chepiao.schedule('g1', function (res) {
   console.log(res);
 });
 
-chepiao.leftTickets('2014-02-26', '上虞北', '南京南', function (res) {
+chepiao.leftTickets('2014-01-11', '上虞北', '南京南', function (res) {
+  console.log(res);
+});
+
+chepiao.flights('2014-01-11', '南京', '北京', function (res) {
   console.log(res);
 });
